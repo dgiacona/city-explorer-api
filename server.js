142c25507590f8db4833144f136f4c8906a6069e
Dominic Giacona <dominic@exobyte.io>
2022-06-25 01:05:49 -0400

    lab 10 set up w/ updated readme diagram

--- server.js
+++ server.js
@@ -53,3 +53,26 @@ app.use((error, req, res, next) => {
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
+
+
+//Starter Code for Server.js
+
+// require('dotenv');
+// const express = require('express');
+// const cors = require('cors');
+
+// const weather = require('./modules/weather.js');
+// const app = express();
+
+// app.get('/weather', weatherHandler);
+
+// function weatherHandler(request, response) {
+//   const { lat, lon } = request.query;
+//   weather(lat, lon)
+//   .then(summaries => response.send(summaries))
+//   .catch((error) => {
+//     console.error(error);
+//     response.status(200).send('Sorry. Something went wrong!')
+//   });
+// }  
